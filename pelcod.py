98c4670fed46ad337f8ef8e371deca2de5bffa80
avrzll <106380642+avrzll@users.noreply.github.com>
2022-09-10 10:28:53 +0700

    Add files via upload

---
+++ pelcod.py
@@ -0,0 +1,49 @@
+#belajar elemen dasar python
+print("P")
+
+BarangKecil = "flashdisk"
+print("saya punya "+BarangKecil)
+
+#type data di python : 1.String 2.int 3.float 4.boolean
+
+#string
+string = 'ini tipe data string'
+print(string)
+
+#tipe data int
+int = 10
+print("ini angka ",+int)
+
+#tipe data float
+float = 10,5
+
+#tipe data boolean (false or true)
+print("tipe data boolean=", + 10 > 9)
+
+a = 10
+b = 17
+
+print('apakah a sama dengan b? = ', +a == b) 
+
+#operator aritmatika { (+), (-), (*), (/), (**), (%) }
+#penjumlahan (+)
+a = 16
+b = 8
+print('hasil penjumlahan ini adalah = ', + a+b)
+
+#pengurangan (-)
+print('hasil pengurangan ini adalah = ', + a-b)
+
+#perkalian (*)
+print('hasil perkalian ini adalah = ', + a*b)
+
+#pembagian (/)
+print('hasil pembagian ini adalah = ', + a/b)
+
+#kuadrat (**)
+print('hasil kuadrat ini adalah = ', + a**b)
+
+#modulus (%)
+a = 9
+b = 3
+print('hasil modulus atau sisa ini adalah = ', + a%b)
